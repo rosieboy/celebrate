81a94ef4f1daefdba68368d5b8d772672d6ea57e
Anders <androsen@gmail.com>
2020-11-18 14:29:17 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,10 @@
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ * 
+ * Google Analytics:
+ * https://www.gatsbyjs.com/plugins/gatsby-plugin-google-analytics/
+ * 
  */
 
 // In your gatsby-config.js
@@ -13,7 +17,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-2TV0HE8HCX",
+        trackingId: "253330304",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
@@ -24,12 +28,8 @@ module.exports = {
         exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
         // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        variationId: 0,
         // Defers execution of google analytics script after page load
         defer: false,
         // Any additional optional fields
