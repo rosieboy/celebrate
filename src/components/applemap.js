088eda2f7c7fbde511c854ce94a46830b1df9c54
Anders <androsen@gmail.com>
2020-12-17 10:27:57 +0100

    Update applemap.js

--- src/components/applemap.js
+++ src/components/applemap.js
@@ -7,7 +7,7 @@ function AppleMapButton({ long, lat, marker }) {
         <a href={"http://maps.apple.com/?&q=" + marker + "&ll=" + lat + "," + long + "&z=17&t=s"} 
         className="w-full flex items-center justify-center mt-4 px-8 py-3 
         border border-transparent text-base leading-6 font-medium 
-        rounded-md text-white bg-indigo-600 hover:text-gray-300 
+        rounded-md text-white bg-indigo-600 hover:text-indigo-400 
         hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo 
         focus:border-indigo-300 transition duration-150 ease-in-out 
         md:py-4 md:text-lg md:px-10">
